872afd4abc1ec52a85e5376712905a3b17e70979
DESKTOP-I5BUBU5\Bartek <chladek13@gmail.com>
2021-09-14 12:17:36 +0200

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+import styles from '@/styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>TypeScript starter for Next.js</title>
+        <meta
+          name="description"
+          content="TypeScript starter for Next.js that includes all you need to build amazing apps"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>Hello world</div>
+    </div>
+  );
+}
