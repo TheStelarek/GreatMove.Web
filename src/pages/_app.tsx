3837495441b4696bbfcf7f40bf4649c401afda1a
Bartosz Chlad <chladek13@gmail.com>
2021-10-21 04:46:39 +0200

    feat: add nprogress (#40)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
+import Router from 'next/router';
 import React from 'react';
+import NProgress from 'nprogress';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from '@/store/index';
 import AuthGuard from '@/components/authGuard/AuthGuard';
-import '@/styles/global.scss';
 import { NextApplicationPage } from '@/utils/types/NextApplicationPage';
+import '@/styles/global.scss';
+import '@/styles/nprogress.scss';
+
+NProgress.configure({ showSpinner: false });
+Router.events.on(`routeChangeStart`, NProgress.start);
+Router.events.on(`routeChangeError`, NProgress.done);
+Router.events.on(`routeChangeComplete`, NProgress.done);
 
 type AppPropsWithLayout = AppProps & {
   Component: NextApplicationPage;
