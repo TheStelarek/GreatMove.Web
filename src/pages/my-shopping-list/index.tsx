8c76b710aa0f39bbc5aca28b0ba0d411e7be1c1e
Bartosz Chlad <chladek13@gmail.com>
2021-11-03 05:00:59 +0100

    refactor: change name of shopping list, change styles and icons (#52)

--- src/pages/my-shopping-list/index.tsx
+++ src/pages/my-shopping-list/index.tsx
@@ -1,47 +1,43 @@
-import React, { ReactElement, useEffect, useRef, useState } from 'react';
+import { ReactElement, useEffect, useState, useRef } from 'react';
 import ReactToPrint from 'react-to-print';
 import { usePDF } from '@react-pdf/renderer';
-import { NextApplicationPage } from '@/utils/types/NextApplicationPage';
 import Layout from '@/components/core/layout/Layout';
-import Save from '@/public/myShoppingList/save.svg';
-import Print from '@/public/myShoppingList/print.svg';
-import Trash from '@/public/myShoppingList/trash.svg';
-import Empty from '@/public/myShoppingList/empty.svg';
 import { shoppingListSelector, deleteProduct, clearState } from '@/store/shoppingList/ShoppingListSlice';
 import { useAppSelector } from '@/store/hooks/useAppSelector';
+import { useAppDispatch } from '@/store/hooks/useAppDispatch';
 import ShoppingListTemplate from '@/utils/printTemplates/shoppingListTemplate/ShoppingListTemplate';
 import { Ingredient } from '@/utils/types/Ingredient';
 import chunks from '@/utils/functions/chunks';
-import { useAppDispatch } from '@/store/hooks/useAppDispatch';
-import styles from './ShoppingList.module.scss';
+import styles from '@/pages/my-shopping-list/MyShoppingList.module.scss';
+import Save from '@/public/my-shopping-list/save.svg';
+import Print from '@/public/my-shopping-list/print.svg';
+import Trash from '@/public/my-shopping-list/trash.svg';
+import Empty from '@/public/my-shopping-list/empty.svg';
+import Remove from '@/public/my-shopping-list/remove.svg';
 
-const ShoppingList: NextApplicationPage = () => {
+const MyShoppingList = () => {
    const [dividedProducts, setDividedProducts] = useState<Array<Ingredient[]>>([]);
-   const componentRef = React.useRef(null);
-   const dispatch = useAppDispatch();
-
-   const clearShoppingList = () => dispatch(clearState());
-
-   const { products } = useAppSelector(shoppingListSelector);
-
    const [pdf, updatePdf] = usePDF({
       document: ShoppingListTemplate({ products: dividedProducts }),
    });
+   const { products } = useAppSelector(shoppingListSelector);
+   const dispatch = useAppDispatch();
+   const componentRef = useRef(null);
 
    useEffect(() => {
       setDividedProducts(chunks(products, 30));
    }, [products]);
 
-   const [showMe, setShowMe] = useState(false);
+   useEffect(updatePdf, [dividedProducts, updatePdf]);
 
-   useEffect(updatePdf, [dividedProducts]);
+   const clearShoppingList = () => dispatch(clearState());
 
    return (
-      <div className={styles.container}>
-         <div className={styles.header}>
+      <div className={styles.shoppingListContainer}>
+         <div className={styles.shoppingListHeader}>
             <h1 className={styles.title}>My shopping list</h1>
             {!!products.length && (
-               <div className={styles.buttons}>
+               <div className={styles.buttonsWrapper}>
                   <div ref={componentRef} className={styles.hidden}>
                      <ShoppingListTemplate products={dividedProducts} />
                   </div>
@@ -53,12 +49,8 @@ const ShoppingList: NextApplicationPage = () => {
                      )}
                      content={() => componentRef.current}
                   />
-                  <a href={pdf.url as string} download="shopping-list.pdf">
-                     <button
-                        type="button"
-                        className={styles.saveButton}
-                        disabled={!!(pdf.loading || pdf.error || !products.length)}
-                     >
+                  <a href={pdf.url as string} download="my-shopping-list.pdf">
+                     <button type="button" className={styles.saveButton} disabled={!!(pdf.loading || pdf.error)}>
                         <Save className={styles.icon} />
                      </button>
                   </a>
@@ -68,27 +60,26 @@ const ShoppingList: NextApplicationPage = () => {
 
          <div className={styles.productsContainer}>
             {products.length === 0 && (
-               <div className={styles.productsEmpty}>
-                  <Empty className={styles.emptyIcon} />
-                  <p className={styles.emptyText}>
-                     Your shopping list is empty. Check our recipes and add ingredients!
-                  </p>
+               <div className={styles.emptyProducts}>
+                  <Empty />
+                  <p className={styles.info}>Your shopping list is empty. Check our recipes and add ingredients!</p>
                </div>
             )}
-            <ul className={styles.products}>
+            <ul className={styles.productsList}>
                {!!products.length &&
                   products.map(({ id, name, weight }, index) => (
                      <li className={styles.product} key={id}>
-                        <div className={styles.productNumber}>{index + 1}. </div>
-                        <div className={styles.productName}>{name}</div>
-                        <div className={styles.productWeight}> {`${weight} g`}</div>
+                        <div className={styles.productNumber}>{index + 1}.</div>
+                        <div className={styles.productName}>
+                           {name} <span className={styles.productWeight}>{`${weight}g`}</span>
+                        </div>
                         <div className={styles.productActions}>
                            <button
                               type="button"
                               className={styles.removesButton}
                               onClick={() => dispatch(deleteProduct(id))}
                            >
-                              <span className={styles.removeButton} />
+                              <Remove />
                            </button>
                         </div>
                      </li>
@@ -105,9 +96,8 @@ const ShoppingList: NextApplicationPage = () => {
       </div>
    );
 };
+export default MyShoppingList;
 
-export default ShoppingList;
-
-ShoppingList.getLayout = function getLayout(page: ReactElement) {
+MyShoppingList.getLayout = function getLayout(page: ReactElement) {
    return <Layout>{page}</Layout>;
 };
