ed05189a9908f11365195e03133906d76469eedf
Dennis Buk <thestelarek97@gmail.com>
2021-11-08 14:33:28 +0100

    feat: add menu to profile page

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -16,38 +16,70 @@ const Profile = () => (
       <div className={styles.imageContainer}>
          <Image src="/profile/banner.jpg" alt="Picture of the author" layout="fill" objectFit="cover" />
       </div>
-      <div className={styles.profileContainer}>
-         <div className={styles.avatar}>
-            <Image src="/profile/avatar.png" alt="Picture of the author" layout="fill" objectFit="cover" />
-         </div>
-         <div className={styles.name}>
-            <p>Dennis Buk</p>
-         </div>
-         <div className={styles.tag}>
-            <p>@Hickari</p>
-         </div>
-         <div>
-            <Button size="small" leftIcon={<Follow className={styles.icon} />} borderRadius={5} variant="ghost-primary">
-               Follow
-            </Button>
-         </div>
-         <div className={styles.informationContainer}>
-            <div className={styles.informationWrapper}>
-               <Location className={styles.icon} /> <p>Warszawa</p>
+      <div className={styles.botContainer}>
+         <div className={styles.profileContainer}>
+            <div className={styles.avatar}>
+               <Image src="/profile/avatar.png" alt="Picture of the author" layout="fill" objectFit="cover" />
             </div>
-            <div className={styles.informationWrapper}>
-               <Website className={styles.icon} /> <p>Hickari.PL</p>
+            <div className={styles.name}>
+               <p>Dennis Buk</p>
             </div>
-            <div className={styles.informationWrapper}>
-               <Birthday className={styles.icon} /> <p>17.12.1997</p>
+            <div className={styles.tag}>
+               <p>@Hickari</p>
+            </div>
+            <div>
+               <Button
+                  size="small"
+                  leftIcon={<Follow className={styles.icon} />}
+                  borderRadius={5}
+                  variant="ghost-primary"
+               >
+                  Follow
+               </Button>
+            </div>
+            <div className={styles.informationContainer}>
+               <div className={styles.informationWrapper}>
+                  <Location className={styles.icon} /> <p>Warszawa</p>
+               </div>
+               <div className={styles.informationWrapper}>
+                  <Website className={styles.icon} /> <p>Hickari.PL</p>
+               </div>
+               <div className={styles.informationWrapper}>
+                  <Birthday className={styles.icon} /> <p>17.12.1997</p>
+               </div>
+            </div>
+            <div className={styles.SocialContainer}>
+               <p className={styles.socialText}>Social</p>
+               <div className={styles.socialWrapper}>
+                  <Facebook className={styles.socialIcon} />
+                  <Twitter className={styles.socialIcon} />
+                  <Instagram className={styles.socialIcon} />
+               </div>
             </div>
          </div>
-         <div className={styles.SocialContainer}>
-            <p className={styles.socialText}>Social</p>
-            <div className={styles.socialWrapper}>
-               <Facebook className={styles.socialIcon} />
-               <Twitter className={styles.socialIcon} />
-               <Instagram className={styles.socialIcon} />
+         <div className={styles.rightBox}>
+            <div className={styles.menuContainer}>
+               <p className={styles.menuText}>
+                  <button className={styles.menuButton}>Discussion</button>
+               </p>
+               <p className={styles.menuText}>
+                  <p className={styles.menuText}>
+                     <button className={styles.menuButton}>Training</button>
+                  </p>
+               </p>
+               <p className={styles.menuText}>
+                  <p className={styles.menuText}>
+                     <button className={styles.menuButton}>Achivments</button>
+                  </p>
+               </p>
+               <p className={styles.menuText}>
+                  <p className={styles.menuText}>
+                     <button className={styles.menuButton}>Recipes</button>
+                  </p>
+               </p>
+            </div>
+            <div className={styles.commentContainer}>
+               <div className={styles.commentBox}>XD</div>
             </div>
          </div>
       </div>
