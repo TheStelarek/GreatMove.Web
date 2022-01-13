96e99fef42c7341faa12a81a11a577ef7bed4812
Dennis Buk <thestelarek97@gmail.com>
2022-01-13 13:19:03 +0100

    refactor: move components to the new architecture

---
+++ src/features/settings/components/SettingsContainer.tsx
@@ -0,0 +1,68 @@
+import React, { ReactElement } from 'react';
+import SettingsIcon from '@/public/settings/settings.svg';
+import HelpIcon from '@/public/settings/help.svg';
+import NotificationsIcon from '@/public/settings/notification.svg';
+import EditProfileIcon from '@/public/settings/pen.svg';
+import Arrow from '@/public/settings/arrow.svg';
+import SecurityIcon from '@/public/settings/security.svg';
+import Input from '@/components/core/input/Input';
+import styles from './SettingsContainer.module.scss';
+
+const SettingsContainer = () => (
+   <div className={styles.Container}>
+      <div className={styles.LeftBox}>
+         <div className={styles.LeftMenu}>
+            <div className={styles.IconContainer}>
+               <SettingsIcon className={styles.SettingsIcon} />
+               <div className={styles.IconText}>Settings</div>
+            </div>
+            <div className={styles.CategoryContainer}>
+               <div className={styles.EditProfileContainter}>
+                  <EditProfileIcon className={styles.Icon} />
+                  Edit Profile
+                  <Arrow className={styles.Arrow} />
+               </div>
+               <div className={styles.NotificationsContainer}>
+                  <NotificationsIcon className={styles.Icon} />
+                  Notifications
+               </div>
+               <div className={styles.SecurityContainer}>
+                  <SecurityIcon className={styles.Icon} />
+                  Security and Login
+               </div>
+               <div className={styles.HelpContainer}>
+                  <HelpIcon className={styles.Icon} />
+                  Help and support
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div className={styles.RightBox}>
+         <p className={styles.title}>Edit Profile</p>
+         <div className={styles.inputsContainer}>
+            <div className={styles.inputsWrapper}>
+               <Input variant="gray" type="text" label="Name" placeholder="Your Name" size="big" />
+               <Input variant="gray" type="text" label="Name" placeholder="Your Name" size="big" />
+            </div>
+            <div className={styles.inputsWrapper}>
+               <Input variant="gray" type="text" label="Name" placeholder="Your Name" size="big" />
+               <Input variant="gray" type="text" label="Name" placeholder="Your Name" size="big" />
+            </div>
+            <div className={styles.inputsWrapper}>
+               <Input variant="gray" type="text" label="Name" placeholder="Your Name" size="big" />
+               <Input variant="gray" type="text" label="Name" placeholder="Your Name" size="big" />
+            </div>
+            <div className={styles.inputsWrapper}>
+               <Input variant="gray" type="text" label="Name" placeholder="Your Name" size="big" />
+               <Input variant="gray" type="text" label="Name" placeholder="Your Name" size="big" />
+            </div>
+            <div className={styles.inputsWrapper}>
+               <Input variant="gray" type="text" label="Name" placeholder="Your Name" size="big" />
+               <Input variant="gray" type="text" label="Name" placeholder="Your Name" size="big" />
+            </div>
+         </div>
+      </div>
+   </div>
+);
+export default SettingsContainer;
--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,73 +1,11 @@
-import React, { ReactElement } from 'react';
-import Layout from '@/components/core/layout/Layout';
-import SettingsIcon from '@/public/settings/settings.svg';
-import HelpIcon from '@/public/settings/help.svg';
-import NotificationsIcon from '@/public/settings/notification.svg';
-import EditProfileIcon from '@/public/settings/pen.svg';
-import Arrow from '@/public/settings/arrow.svg';
-import SecurityIcon from '@/public/settings/security.svg';
-import Input from '@/components/core/input/Input';
-import styles from './Settings.module.scss';
+import { ReactElement } from 'react';
+import MainLayout from '@/layouts/mainLayout/MainLayout';
+import SettingsContainer from '@/features/settings/components/SettingsContainer';
 
-const Settings = () => (
-   <div className={styles.Container}>
-      <div className={styles.LeftBox}>
-         <div className={styles.LeftMenu}>
-            <div className={styles.IconContainer}>
-               <SettingsIcon className={styles.SettingsIcon} />
-               <div className={styles.IconText}>Settings</div>
-            </div>
-            <div className={styles.CategoryContainer}>
-               <div className={styles.EditProfileContainter}>
-                  <EditProfileIcon className={styles.Icon} />
-                  Edit Profile
-                  <Arrow className={styles.Arrow} />
-               </div>
-               <div className={styles.NotificationsContainer}>
-                  <NotificationsIcon className={styles.Icon} />
-                  Notifications
-               </div>
-               <div className={styles.SecurityContainer}>
-                  <SecurityIcon className={styles.Icon} />
-                  Security and Login
-               </div>
-               <div className={styles.HelpContainer}>
-                  <HelpIcon className={styles.Icon} />
-                  Help and support
-               </div>
-            </div>
-         </div>
-      </div>
+const MyShoppingList = () => <SettingsContainer />;
 
-      <div className={styles.RightBox}>
-         <p className={styles.title}>Edit Profile</p>
-         <div className={styles.inputsContainer}>
-            <div className={styles.inputsWrapper}>
-               <Input variant="gray" type="text" label="Name" placeholder="Your Name" size="big" />
-               <Input variant="gray" type="text" label="Name" placeholder="Your Name" size="big" />
-            </div>
-            <div className={styles.inputsWrapper}>
-               <Input variant="gray" type="text" label="Name" placeholder="Your Name" size="big" />
-               <Input variant="gray" type="text" label="Name" placeholder="Your Name" size="big" />
-            </div>
-            <div className={styles.inputsWrapper}>
-               <Input variant="gray" type="text" label="Name" placeholder="Your Name" size="big" />
-               <Input variant="gray" type="text" label="Name" placeholder="Your Name" size="big" />
-            </div>
-            <div className={styles.inputsWrapper}>
-               <Input variant="gray" type="text" label="Name" placeholder="Your Name" size="big" />
-               <Input variant="gray" type="text" label="Name" placeholder="Your Name" size="big" />
-            </div>
-            <div className={styles.inputsWrapper}>
-               <Input variant="gray" type="text" label="Name" placeholder="Your Name" size="big" />
-               <Input variant="gray" type="text" label="Name" placeholder="Your Name" size="big" />
-            </div>
-         </div>
-      </div>
-   </div>
-);
-export default Settings;
+export default MyShoppingList;
 
-Settings.getLayout = function getLayout(page: ReactElement) {
-   return <Layout fullWidth>{page}</Layout>;
+MyShoppingList.getLayout = function getLayout(page: ReactElement) {
+   return <MainLayout fullWidth>{page}</MainLayout>;
 };
