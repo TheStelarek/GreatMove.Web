ac9f1653d8b44f7f4d627a15bfedcb53e4c028fc
DESKTOP-I5BUBU5\Bartek <chladek13@gmail.com>
2022-01-13 13:20:29 +0100

    feat: display spinner and error in recipe page

--- src/features/recipe/containers/recipeContainer/RecipeContainer.tsx
+++ src/features/recipe/containers/recipeContainer/RecipeContainer.tsx
@@ -2,14 +2,32 @@ import { useRouter } from 'next/router';
 import { useGetRecipeByIdQuery } from '@/features/recipe/api/recipesApi';
 import { skipToken } from '@reduxjs/toolkit/query';
 import RecipeWrapper from '@/features/recipe/components/recipe/recipeWrapper/RecipeWrapper';
+import Spinner from '@/components/core/spinner/Spinner';
+import { ErrorType } from '@/utils/types/ErrorType';
+import styles from '@/features/recipe/containers/recipeContainer/RecipeContainer.module.scss';
 
 const RecipeContainer = () => {
    const router = useRouter();
-   const { data, isLoading, isFetching, isError } = useGetRecipeByIdQuery(
+   const { data, isLoading, isError, error } = useGetRecipeByIdQuery(
       router.query.id ? (router.query.id as string) : skipToken,
    );
 
-   return <div>{data && <RecipeWrapper recipe={data} />}</div>;
+   return (
+      <>
+         {isLoading && (
+            <div className={styles.wrapper}>
+               <Spinner size="extra-large" variant="ghost-secondary" />
+            </div>
+         )}
+         {isError && error && (
+            <div className={styles.wrapper}>
+               <p className="error">{(error as ErrorType).data.message}</p>
+            </div>
+         )}
+
+         {data && !isError && <RecipeWrapper recipe={data} />}
+      </>
+   );
 };
 
 export default RecipeContainer;
